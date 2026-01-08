c85d17f620782f1a70a986bc3dce6f4f2c6d48d2
bellhopreshoot64 <rail.whiles_9z@icloud.com>
2026-01-08 17:59:02 +0100

    Commit1

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('catalogue/', include('apps.catalog.urls')), # Legacy reference if any
     path('', include('apps.catalog.urls')), # Main entry
 
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
